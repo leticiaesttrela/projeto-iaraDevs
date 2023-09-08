78de3d081366846abb65d3633f61dec9273cf431
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-07 23:19:24 -0300

    feat: adicionando rota de campeonatos ao server

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import {server} from "../src/server/server"
 import { championshipsRouter } from "./routes/championshipsRouter";
 import 'dotenv/config'
 import { playerRouter } from "./routes/playersRouter";
+import { teamRouter } from "./routes/teamsRouter";
 
 server.listen(process.env.PORT || 5000, () => {
     console.log('Servidor rodando na porta 3333');
 })
 
 server.use(playerRouter);
-server.use(championshipsRouter)
+server.use(championshipsRouter);
+server.use(teamRouter)
